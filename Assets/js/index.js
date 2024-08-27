7da3b18e05eea8354f55716e6bde328174413f0b
tanner johnson <tannerjohnson08@gmail.com>
2024-08-27 00:52:43 -0500

    finishing for the night

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,1 +1,41 @@
 // key: 83289a961f66ea21035d3f375d92539e
+
+
+function handleSearch(input){
+    // console.log(input)
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=83289a961f66ea21035d3f375d92539e`
+    const response = getResponse(url);
+    console.log(response)
+}
+
+
+function getWeather(locationObj){
+  
+
+
+}
+
+
+function getFormInput(){
+    return document.querySelector("#search-field").value
+}
+
+function getResponse(url) {
+
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        return data;    
+      });
+    
+  }
+
+
+document.querySelector("#search-btn").addEventListener("click", function(e){
+    e.preventDefault();
+    handleSearch(getFormInput())
+    document.querySelector("#search-field").value = ''
+})
